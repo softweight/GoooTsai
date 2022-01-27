9adf78b21fcf7880a411411fa30f73f4cdfe9289
softweight <diweird610@gmail.com>
2022-01-27 17:59:37 +0800

    after orange

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from turtle import position
 import discord
+import re
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!') # !Command
@@ -9,25 +10,61 @@ TOKEN = ''
 f = open("TOKEN","r")
 TOKEN = f.readline()
 
-# simple ping pong
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     if message.content == 'ping':
-#         await message.channel.send('pong')
+text_channel_list = []
+mycategory = ''
 
+def check():
+    global mycategory,text_channel_list
+    for channel in bot.get_all_channels():
+        if str(channel.type) == 'text' and channel.category_id == 936150063649030154:    #
+            text_channel_list.append(channel.name)
+            mycategory = channel.category
 
-async def on_ready():
-    print('Login in : ', bot.user)
+    print(text_channel_list)
+
 
 @bot.command()
 async def new(ctx, arg):
+    global mycategory,text_channel_list
 	# await ctx.channel.send("got "+arg)
-    guilds = await bot.fetch_guilds(limit=150).flatten()
-    print(guilds)
-    for i in guilds:
-        channel = await i.create_text_channel(arg)
-        await channel.edit()
+    regex = re.compile(r'\d{4}')
+    if not re.match(regex, arg):
+        await ctx.send("not stock!")
+    else:
+        guilds = await bot.fetch_guilds(limit=150).flatten()
+        for guild in guilds:
+            text_channel_list = []
+            check()
+            if arg not in text_channel_list:
+                channel = await guild.create_text_channel(arg)
+                await channel.edit(category=mycategory)
+                await ctx.send("Done!")
+            else:
+                await ctx.send("already exit...")
+
+alarm_time = '23:33'#24hrs
+channel_id = '51599XXXXX5036697'
+
+@bot.event
+async def on_ready():
+    print('Bot Online.')
+
+alarm_time = '23:33'
+
+async def time_check():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        now = bot.strftime(bot.now(), '%H:%M')
+        channel = bot.get_channel(channel_id)
+        messages = ('Test')
+        if now == alarm_time:
+            
+            time = 90
+        else:
+            time = 1
+
+
+bot.loop.create_task(time_check())
+
 
 bot.run(TOKEN)
